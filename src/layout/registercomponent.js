04ed0240ac5b73ca9ed638f1fa666a924c6e1e04
shikhasingh99 <singhshikha701@gmail.com>
2021-07-27 15:07:10 +0530

    updates

--- src/layout/registercomponent.js
+++ src/layout/registercomponent.js
@@ -21,7 +21,7 @@ export class RegisterComp extends Component {
                 <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Employee</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Employeer</a>
+                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Employer</a>
             </li>
         </ul>
         <div class="tab-content" id="myTabContent">
